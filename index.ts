ec39e13859ac2ae4e15e919efd2439e10ee5e104
frehml <frederic.hamelink@hotmail.com>
2022-06-02 15:45:19 +0000

    index.ts update

--- index.ts
+++ index.ts
@@ -8,29 +8,31 @@ interface vdf {
 }
 
 const vdf: vdf = require(".");
-import { performance } from "perf_hooks";
 
 function testVdf() {
   const buffer = new Uint8Array(Buffer.from("hello testing"));
   const iterations = 100;
 
-  var startTime = performance.now();
   const proof: Uint8Array = vdf.vdfProveWesolowski(buffer, iterations);
-  var midTime = performance.now();
+  console.log(proof);
+
   const valid: boolean = vdf.vdfVerifyWesolowski(
     buffer,
     iterations,
     Buffer.from(proof)
   );
-  var endTime = performance.now();
-
   console.log("valid: ", valid);
-  console.log(
-    `Call to vdfProveWesolowski took ${midTime - startTime} milliseconds`
-  );
-  console.log(
-    `Call to vdfVerifyWesolowski took ${endTime - midTime} milliseconds`
-  );
+}
+
+function timeTest() {
+  console.log(`Testing performance, aa 90000 iterations`);
+  const buffer = new Uint8Array(Buffer.from("aa", "hex"));
+  const iterations = 90000;
+
+  console.time("prove");
+  vdf.vdfProveWesolowski(buffer, iterations);
+  console.timeEnd("prove");
 }
 
 testVdf();
+timeTest();
